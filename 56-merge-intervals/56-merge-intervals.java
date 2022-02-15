1408bc6b62657b4cad409fbb5e3a26dff097cda3
Sanjay S S <sanjay.india96@gmail.com>
2022-02-15 13:12:11 +0530

    Time: 13 ms (48.84%), Space: 55.3 MB (13.38%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        
+        if (intervals.length == 1) 
+            return intervals;
+        
+        //Sort the intervals based on the start value
+        Arrays.sort(intervals, (x, y) -> Integer.compare(x[0], y[0]));
+        
+        var result = new ArrayList<int[]>();
+        var newInterval = intervals[0];
+        result.add(newInterval);
+        
+        for (int[] interval : intervals) {
+            
+            // Check if intervals are overlapping 
+            // if yes, move the end if needed
+            if (interval[0] <= newInterval[1]) {
+                newInterval[1] = Math.max(newInterval[1], interval[1]);
+            }
+            // Not overlapping
+            // Add to the list
+            else {
+                newInterval = interval;
+				result.add(newInterval);
+            }
+        }
+        
+        return result.toArray(new int[result.size()][]);
+    }
+}
