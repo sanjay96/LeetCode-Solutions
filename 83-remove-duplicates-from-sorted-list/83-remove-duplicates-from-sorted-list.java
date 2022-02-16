296fae44381bca32826ebac7832c439a3e712135
Sanjay S S <sanjay.india96@gmail.com>
2022-02-17 00:47:20 +0530

    Time: 1 ms (62.97%), Space: 44.3 MB (14.13%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
@@ -9,7 +9,20 @@
  * }
  */
 class Solution {
+    // Using one pointer
     public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val)
+                current.next = current.next.next;
+            else
+                current = current.next;
+        }
+        return head;
+    }
+    
+    // Using fast and slow pointers
+    /**public ListNode deleteDuplicates(ListNode head) {
         if (head == null) return head;
         ListNode newHead = new ListNode(0);
         newHead.next = head;
@@ -30,4 +43,5 @@ class Solution {
         return newHead.next;
         
     }
+    **/
 }
