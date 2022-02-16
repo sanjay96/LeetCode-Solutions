2be460cc33ca96d9e53a7f2aeab97d5eb0d64e8e
Sanjay S S <sanjay.india96@gmail.com>
2022-02-16 12:26:42 +0530

    Time: 11 ms (75.40%), Space: 45 MB (67.59%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -5,29 +5,13 @@ class Solution {
         for (int x : nums) {
             map.put(x, map.getOrDefault(x, 0) + 1);
         }
-        List<Integer> l = new ArrayList<>();
+        List<Integer> frequencyList = new ArrayList<>();
         map.entrySet().stream()
                 .sorted((k1, k2) -> -k1.getValue().compareTo(k2.getValue()))
-                .forEach(a -> l.add(a.getKey()));
+                .forEach(a -> frequencyList.add(a.getKey()));
         int[] answer = new int[k];
         for (int i = 0; i < k; i++)
-            answer[i] = l.get(i);
+            answer[i] = frequencyList.get(i);
         return answer;
     }
-//     public int[] topKFrequent(int[] nums, int k) {
-//         var map = new HashMap<Integer, Integer>();
-//         for (int x : nums) {
-//             map.put(x, map.getOrDefault(x, 0) + 1);
-//         }
-//         List<Integer> result = new ArrayList<>();
-//         for (int x : map.keySet()) {
-//             if (map.get(x) >= k) result.add(x);
-//         }
-//         int[] answer = new int[result.size()];
-        
-//         for (int i = 0;  i < result.size(); i++) {
-//             answer[i] = result.get(i);    
-//         }
-//         return answer;
-//     }
 }
