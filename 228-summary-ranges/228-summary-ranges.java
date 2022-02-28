6697049179e0a1cba36fb4230ced140407a17b5a
Sanjay S S <sanjay.india96@gmail.com>
2022-02-28 23:37:54 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (12.77%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.java
+++ 228-summary-ranges/228-summary-ranges.java
@@ -1,20 +1,44 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
-        if (nums == null || nums.length == 0)
-            return new ArrayList<String>();
         List<String> result = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        
         for (int i = 0; i < nums.length; i++) {
-            int start = nums[i];
-            while (i + 1 < nums.length && nums[i] + 1 == nums[i+1]) {
-                i++;
+            sb.setLength(0);
+            int index = i;
+            while (index + 1 < nums.length && nums[index] + 1 == nums[index + 1]) {
+                index++;
             }
-            if (start != nums[i]) {
-                result.add("" + start + "->" + nums[i]);
-            }
+            
+            if (index == i) {
+                sb.append(nums[i]);
+            } 
             else {
-                result.add("" + start);
+                sb.append(nums[i]).append('-').append('>').append(nums[index]);
             }
+            result.add(sb.toString());
+            i = index;
         }
         return result;
     }
+    
+    // Approach 1
+    // public List<String> summaryRanges(int[] nums) {
+    //     if (nums == null || nums.length == 0)
+    //         return new ArrayList<String>();
+    //     List<String> result = new ArrayList<>();
+    //     for (int i = 0; i < nums.length; i++) {
+    //         int start = nums[i];
+    //         while (i + 1 < nums.length && nums[i] + 1 == nums[i+1]) {
+    //             i++;
+    //         }
+    //         if (start != nums[i]) {
+    //             result.add("" + start + "->" + nums[i]);
+    //         }
+    //         else {
+    //             result.add("" + start);
+    //         }
+    //     }
+    //     return result;
+    // }
 }
