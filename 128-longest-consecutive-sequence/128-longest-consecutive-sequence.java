00d2a1c7a2b84e85008cf0a849ab38051add8a54
Sanjay S S <sanjay.india96@gmail.com>
2022-03-07 02:48:48 +0530

    Time: 31 ms (71.05%), Space: 61 MB (52.40%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -2,25 +2,51 @@ class Solution {
     public int longestConsecutive(int[] nums) {
         if (nums == null || nums.length == 0) 
             return 0;
-        Set<Integer> set = new HashSet<>();
         
+        Set<Integer> set = new HashSet<>();
         for (int x : nums) 
             set.add(x);
         
-        int count = 0;
-        int longestSequence = 0;
+        int currentStreak = 0;
+        int longestStreak = 0;
         
-        for (int i = 0; i < nums.length; i++) {
-            //Check if the number is the start of the sequence
-            if (set.contains(nums[i]-1)) 
-                continue;
-            else {
-                count = 1;
-                while (set.contains(nums[i] + count))
-                    count++;
-                longestSequence = Math.max(longestSequence, count);
+        for (int num : set) {
+            if (!set.contains(num + 1)) {
+                while (set.contains(num)) {
+                    currentStreak++;
+                    num--;
+                }
+                longestStreak = Math.max(longestStreak, currentStreak);
+                currentStreak = 0;
             }
         }
-        return longestSequence;
+        return longestStreak;
     }
 }
+
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         if (nums == null || nums.length == 0) 
+//             return 0;
+//         Set<Integer> set = new HashSet<>();
+        
+//         for (int x : nums) 
+//             set.add(x);
+        
+//         int count = 0;
+//         int longestSequence = 0;
+        
+//         for (int i = 0; i < nums.length; i++) {
+//             //Check if the number is the start of the sequence
+//             if (set.contains(nums[i]-1)) 
+//                 continue;
+//             else {
+//                 count = 1;
+//                 while (set.contains(nums[i] + count))
+//                     count++;
+//                 longestSequence = Math.max(longestSequence, count);
+//             }
+//         }
+//         return longestSequence;
+//     }
+// }
