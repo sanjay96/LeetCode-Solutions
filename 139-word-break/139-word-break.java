61f68282ecca6e9812ab9a8fbd608d0b36b7f01e
Sanjay S S <sanjay.india96@gmail.com>
2022-03-09 18:12:09 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (85.18%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,17 +1,42 @@
 class Solution {
+    // Using Depth First Search
     public boolean wordBreak(String s, List<String> wordDict) {
         int n = s.length();
-        boolean[] dp = new boolean[n+1];
-        dp[n] = true;
-        for (int i = n - 1; i >= 0; i--) {
-            for (String w : wordDict) {
-                if ((i + w.length()) <= n && s.substring(i, i + w.length()).equals(w)) {
-                    dp[i] = dp[i + w.length()];
+        boolean[] visited = new boolean[n];
+        return dfs(0, s, visited, wordDict);
+    }
+    
+    public boolean dfs(int index, String s, boolean[] visited, List<String> wordDict) {
+        int n = s.length();
+        if (index == n)
+            return true;
+        if (visited[index] == true) 
+            return false;
+        visited[index] = true;
+        for (String t : wordDict) {
+            if (s.startsWith(t, index)) {
+                if (dfs(index + t.length(), s, visited, wordDict)) {
+                    return true;
                 }
-                if (dp[i] == true)
-                    break;
             }
         }
-        return dp[0];
+        return false;
     }
+    
+    // Using Dynamic Programming
+    // public boolean wordBreak(String s, List<String> wordDict) {
+    //     int n = s.length();
+    //     boolean[] dp = new boolean[n+1];
+    //     dp[n] = true;
+    //     for (int i = n - 1; i >= 0; i--) {
+    //         for (String w : wordDict) {
+    //             if ((i + w.length()) <= n && s.substring(i, i + w.length()).equals(w)) {
+    //                 dp[i] = dp[i + w.length()];
+    //             }
+    //             if (dp[i] == true)
+    //                 break;
+    //         }
+    //     }
+    //     return dp[0];
+    // }
 }
