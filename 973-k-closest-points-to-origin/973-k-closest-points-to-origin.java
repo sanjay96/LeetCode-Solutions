ba27dab83ec1670778a96d6a0582ccfe838b418b
Sanjay S S <sanjay.india96@gmail.com>
2022-03-19 11:55:34 +0530

    Time: 460 ms (5.00%), Space: 112.7 MB (78.10%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,25 +1,73 @@
 class Solution {
-    private int dis(int[] point) {
-        return point[0] * point[0] + point[1] * point[1];
-    }
-    
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((i1, i2) -> {
-            return dis(points[i2]) - dis(points[i1]);
-        });
-        
-        for (int i = 0; i < points.length; i++) {
-            pq.add(i);
-            if (pq.size() > k) {
-                pq.remove();
+        if (points == null || points.length == 0)
+            return new int[0][0];
+        int low = 0, high = points.length - 1;
+        while (low < high) {
+            int index = quickselect(points, low, high);
+            
+            if (index == k) {
+                break;
+            }
+            else if (index < k) {
+                low = index + 1;
+            }
+            else {
+                high = index - 1;
             }
         }
         
-        int[][] result = new int[k][2];
-        for (int i = 0; i < k; i++) {
-            int index = pq.poll();
-            result[i] = points[index];
-        }
-        return result;
+        return Arrays.copyOf(points, k);
     }
+    
+    
+    private int quickselect(int[][] points, int low, int high) {
+        int index = low, pivot = high;
+        
+        int pivotDistance = distance(points[pivot]);
+        
+        for (int i = low; i < high; i++) {
+            if (distance(points[i]) < pivotDistance) {
+                swap(points, i, index);
+                index++;
+            }
+        }
+        swap(points, index, pivot);
+        return index;
+    }
+    
+    private int distance(int[] pair) {
+        return pair[0] * pair[0] + pair[1] * pair[1];
+    }
+    
+    private void swap(int[][] points, int low, int high) {
+        int[] temp = points[low];
+        points[low] = points[high];
+        points[high] = temp;
+    }
+    
+    //Using a Heap
+//     private int dis(int[] point) {
+//         return point[0] * point[0] + point[1] * point[1];
+//     }
+    
+//     public int[][] kClosest(int[][] points, int k) {
+//         PriorityQueue<Integer> pq = new PriorityQueue<>((i1, i2) -> {
+//             return dis(points[i2]) - dis(points[i1]);
+//         });
+        
+//         for (int i = 0; i < points.length; i++) {
+//             pq.add(i);
+//             if (pq.size() > k) {
+//                 pq.remove();
+//             }
+//         }
+        
+//         int[][] result = new int[k][2];
+//         for (int i = 0; i < k; i++) {
+//             int index = pq.poll();
+//             result[i] = points[index];
+//         }
+//         return result;
+//     }
 }
