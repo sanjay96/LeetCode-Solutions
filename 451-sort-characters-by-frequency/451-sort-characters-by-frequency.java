30fda676d7315947fa6cc0d93a1909dbfa487e22
Sanjay S S <sanjay.india96@gmail.com>
2022-03-21 12:29:37 +0530

    Time: 8 ms (95.31%), Space: 42.8 MB (90.92%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
@@ -1,27 +1,56 @@
 class Solution {
+    //Using bucketSort
     public String frequencySort(String s) {
-        Map<Character, int[]> table = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (!table.containsKey(c)) {
-                table.put(c, new int[]{1, i});
-            }
-            else {
-                int[] frequencyAndIndex = table.get(c);
-                frequencyAndIndex[0]++;
-                table.put(c, frequencyAndIndex);
-            }
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        PriorityQueue<Map.Entry<Character, int[]>> pq = new PriorityQueue<>((a,b) -> a.getValue()[0] == b.getValue()[0] ? a.getValue()[1] - b.getValue()[1] : b.getValue()[0] - a.getValue()[0]);
-        
-        pq.addAll(table.entrySet());
-        
+        List<Character>[] bucket = new List[s.length() + 1];
+        for (char key : map.keySet()) {
+            int frequency = map.get(key);
+            if (bucket[frequency] == null) 
+                bucket[frequency] = new ArrayList<>();
+            bucket[frequency].add(key);
+        }
+    
         StringBuilder result = new StringBuilder();
-        while (!pq.isEmpty()) {
-            Map.Entry<Character, int[]> e = pq.poll();
-            for (int i = 0; i < e.getValue()[0]; i++ )
-                result.append(e.getKey());
+        
+        for (int pos = bucket.length - 1; pos >= 0; pos--) {
+            if (bucket[pos] != null)  {
+                for (char c : bucket[pos]) {
+                    for (int i = 0; i < pos; i++) {
+                        result.append(c);
+                    }
+                }
+            }
         }
         return result.toString();
     }
+    
+    // Using a heap
+    // public String frequencySort(String s) {
+//         Map<Character, int[]> table = new HashMap<>();
+//         for (int i = 0; i < s.length(); i++) {
+//             char c = s.charAt(i);
+//             if (!table.containsKey(c)) {
+//                 table.put(c, new int[]{1, i});
+//             }
+//             else {
+//                 int[] frequencyAndIndex = table.get(c);
+//                 frequencyAndIndex[0]++;
+//                 table.put(c, frequencyAndIndex);
+//             }
+//         }
+//         PriorityQueue<Map.Entry<Character, int[]>> pq = new PriorityQueue<>((a,b) -> a.getValue()[0] == b.getValue()[0] ? a.getValue()[1] - b.getValue()[1] : b.getValue()[0] - a.getValue()[0]);
+        
+//         pq.addAll(table.entrySet());
+        
+//         StringBuilder result = new StringBuilder();
+//         while (!pq.isEmpty()) {
+//             Map.Entry<Character, int[]> e = pq.poll();
+//             for (int i = 0; i < e.getValue()[0]; i++ )
+//                 result.append(e.getKey());
+//         }
+//         return result.toString();
+    // }
 }
