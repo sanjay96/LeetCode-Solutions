e4e7808811dbe3becd41a8549e77253d2b6b39cb
Sanjay S S <sanjay.india96@gmail.com>
2022-04-03 18:18:52 +0530

    Time: 3 ms (85.85%), Space: 47.8 MB (64.06%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(result, new ArrayList<>(), nums, new boolean[nums.length]);
+        return result;
+    }
+    
+    private void backtrack(List<List<Integer>> result, List<Integer> current, int[] nums, boolean[] used) {
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+        }
+        else {
+            for (int i = 0; i < nums.length; i++) {
+                if (used[i] || i > 0 && nums[i] == nums[i-1] && !used[i-1]) continue;
+                used[i] = true;
+                current.add(nums[i]);
+                backtrack(result, current, nums, used);
+                used[i] = false;
+                current.remove(current.size() - 1);
+            }
+        }
+    }
+}
