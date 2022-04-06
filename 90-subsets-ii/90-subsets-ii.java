05b547b9a31a65c4db0d01bac88d3959f02ce543
Sanjay S S <sanjay.india96@gmail.com>
2022-04-06 23:20:34 +0530

    Time: 3 ms (41.62%), Space: 44 MB (59.33%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -7,10 +7,12 @@ class Solution {
     }
     
     private void backtrack(List<List<Integer>> result, List<Integer> current, int[] nums, int start) {
-        if (!result.contains(current)) {
-            result.add(new ArrayList<>(current));
-        }
+        
+        result.add(new ArrayList<>(current));
+        
         for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i-1])
+                continue;
             current.add(nums[i]);
             backtrack(result, current, nums, i+1);
             current.remove(current.size() - 1);
