3bd8d3d033a7275a813c2b17d5a296fe262d1eaf
Sanjay S S <sanjay.india96@gmail.com>
2022-04-07 11:25:22 +0530

    Time: 1 ms (98.65%), Space: 41.9 MB (34.21%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,18 +1,29 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        // int result = 0;
-        PriorityQueue<Integer> heap = new PriorityQueue<>((a,b) -> b - a);
-        for (int stone : stones) {
-            heap.add(stone);
+        int n = stones.length;
+        
+        while (n > 1) {
+            Arrays.sort(stones);
+            stones[n-2] = stones[n-1] - stones[n-2];
+            n--;
         }
-        while (true) {
-            int one = heap.poll();
-            if (heap.size() == 0)
-                return one;
-            else {
-                int two = heap.poll();
-                heap.offer(Math.abs(one - two));
-            }
-        }
+        return stones[0];
     }
+    
+    // Heap
+    // public int lastStoneWeight(int[] stones) {
+    //     PriorityQueue<Integer> heap = new PriorityQueue<>((a,b) -> b - a);
+    //     for (int stone : stones) {
+    //         heap.add(stone);
+    //     }
+    //     while (true) {
+    //         int one = heap.poll();
+    //         if (heap.size() == 0)
+    //             return one;
+    //         else {
+    //             int two = heap.poll();
+    //             heap.offer(Math.abs(one - two));
+    //         }
+    //     }
+    // }
 }
